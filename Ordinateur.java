49140e08dc21e15f00def9edbfc57773fb361df0
Tamotaki <tamotaki-pierro@outlook.com>
2026-04-15 14:44:55 +0200

    Constructeur et getter

--- Ordinateur.java
+++ Ordinateur.java
@@ -1,4 +1,6 @@
 public final class Ordinateur {
+
+    //Champs final
     private final String marque;
     private final String processeur;
     private final int ramGo;
@@ -7,5 +9,39 @@ public final class Ordinateur {
     private final String carteGraphique;
     private final double prixEuros;
 
+    //COnstructeur privé
+    private Ordinateur(Builder builder) {
+        this.marque = builder.marque;
+        this.processeur = builder.processeur;
+        this.ramGo = builder.ramGo;
+        this.stockageGo = builder.stockageGo;
+        this.ssd = builder.ssd;
+        this.carteGraphique = builder.carteGraphique;
+        this.prixEuros = builder.prixEuros;
+    }
+
+    //Getters et toString
+    public String getMarque() {
+        return marque;
+    }
+    public String getProcesseur() {
+        return processeur;
+    }
+    public int getRamGo() {
+        return ramGo;
+    }
+    public int getStockageGo() {
+        return stockageGo;
+    }
+    public boolean getSsd() {
+        return ssd;
+    }
+    public String getCarteGraphique() {
+        return carteGraphique;
+    }
+    public double getPrixEuros() {
+        return prixEuros;
+    }
+
     
-        }
+}
