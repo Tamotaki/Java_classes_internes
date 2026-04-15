8584b7faef935913c9d8de34ef9539eb20ee43fe
Tamotaki <tamotaki-pierro@outlook.com>
2026-04-15 14:17:53 +0200

    Début exercice 2

---
+++ Ordinateur.java
@@ -0,0 +1,11 @@
+public final class Ordinateur {
+    private final String marque;
+    private final String processeur;
+    private final int ramGo;
+    private final int stockageGo;
+    private final boolean ssd;
+    private final String carteGraphique;
+    private final double prixEuros;
+
+    
+        }
